0df8333913fe7c3b58d0438ee306605ff1a4cb5f
Bryan Lam <blam.gml@gmail.com>
2024-04-04 14:29:15 -0600

    Added Obsidian.nvim and markdown related plugins and configurations.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,16 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            highlight = { enable = true },
+            highlight = { 
+                enable = true, 
+                --[[
+                disable = {
+                    "markdown"
+                },
+                --]]
+            },
             indent = { enable = true },
+            --ignore_install = { "markdown" }
         })
     end
 }
