eb8617d492595ee5d91493e77a9b3a3234cd82d1
Bryan Lam <blam.gml@gmail.com>
2024-04-05 08:54:53 -0600

    Updated and cleaned the java.lua file.
    Used @pavva91's configuration at
    https://github.com/mfussenegger/nvim-jdtls/wiki/Sample-Configurations to
    resolve issues with starting the dap for java. Uses Mason java-test and
    java-debug-adapter instead of git cloning those projects directly.

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,45 +1,57 @@
-local global = require('global')
-
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
--- Check to see what OS is currently being used.
-
-
-local jdtls = require('jdtls')
-local cmp_nvim_lsp = require('cmp_nvim_lsp')
-
--- Paths
-local jdtls_path = global.config_data_path .. "/mason/packages/jdtls"
-local launcher_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+local global = require("global")
+local jdtls = require("jdtls")
+local jdtls_dap = require("jdtls.dap")
+local jdtls_setup = require("jdtls.setup")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 local root_markers = { "gradlew", "mvnw", ".git", "pom.xml", "build.gradle" }
-local root_dir = vim.fs.dirname(vim.fs.find({ 'pom.xml', '.git' }, { upward = true })[1])
+local root_dir = jdtls_setup.find_root(root_markers)
 
-print(root_dir)
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = global.home_path .. "/.cache/jdtls/workspace" .. project_name
 
-local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local path_to_mason_packages = global.config_data_path .. "/mason/packages"
+
+local jdtls_path = path_to_mason_packages .. "/jdtls"
+local jdtls_debug = path_to_mason_packages .. "/java-debug-adapter"
+local jdtls_jtest = path_to_mason_packages .. "/java-test"
+
+local launcher_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+print(launcher_jar)
+
+local bundles = {
+    vim.fn.glob(jdtls_debug .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
+}
+
+print( vim.inspect(bundles))
+
+vim.list_extend(bundles, vim.split(vim.fn.glob(jdtls_jtest .. "/extensions/server/*.jar", true), "\n"))
+--local root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1])
 
 -- Completions
-local client_capabilities = vim.lsp.protocol.make_client_capabilities()
-local capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
+--local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+--local capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
 
 local function get_config_dir()
-  -- Unlike some other programming languages (e.g. JavaScript)
-  -- lua considers 0 truthy!
-  if vim.fn.has('linux') == 1 then
-    return 'config_linux'
-  elseif vim.fn.has('mac') == 1 then
-    return 'config_mac'
-  else
-    return 'config_win'
-  end
+    -- Unlike some other programming languages (e.g. JavaScript)
+    -- lua considers 0 truthy!
+    if vim.fn.has("linux") == 1 then
+        return "config_linux"
+    elseif vim.fn.has("mac") == 1 then
+        return "config_mac"
+    else
+        return "config_win"
+    end
 end
 
 local config = {
-	settings = {
-		["java.format.settings.url"] = "https://gist.githubusercontent.com/fbricon/c5960a86f0a21405a61b79c0f1b4a0fc/raw/16b9b75077d08ae8b36706718a8393268d2ea5d2/formatter.xml",
+    settings = {
+        ["java.format.settings.url"] =
+        "https://gist.githubusercontent.com/fbricon/c5960a86f0a21405a61b79c0f1b4a0fc/raw/16b9b75077d08ae8b36706718a8393268d2ea5d2/formatter.xml",
         --[[
         java = {
           format = {
@@ -54,68 +66,77 @@ local config = {
         --]]
     },
     --capabilities = capabilities,
-	cmd = {
-		-- 💀
-		"java", -- or '/path/to/java17_or_newer/bin/java'
-		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
-		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-		"-Dosgi.bundles.defaultStartLevel=4",
-		"-Declipse.product=org.eclipse.jdt.ls.core.product",
-		"-Dlog.protocol=true",
-		"-Dlog.level=ALL",
-		"-Xmx1g",
-		"--add-modules=ALL-SYSTEM",
-		"--add-opens",
-		"java.base/java.util=ALL-UNNAMED",
-		"--add-opens",
-		"java.base/java.lang=ALL-UNNAMED",
-		"-jar", launcher_jar,
-		"-configuration", vim.fs.normalize(jdtls_path .. "/" .. get_config_dir()),
-		"-data",
-		vim.fs.normalize(global.home_path .. "/code/java/workspaces/" .. workspace_dir),
-	},
+    cmd = {
+        -- 💀
+        "java", -- or '/path/to/java17_or_newer/bin/java'
+        -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        launcher_jar,
+        "-configuration",
+        vim.fs.normalize(jdtls_path .. "/" .. get_config_dir()),
+        "-data",
+        workspace_dir
+    },
 
-    root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1]),
-	init_options = {
-		--bundles = {},
-        extendedCapabilities = jdtls.extendedCapabilities,
-	},
+    --root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1]),
+    root_dir = require("jdtls.setup").find_root(root_markers),
 }
 
-config.on_attach = function(client, bufnr)
-    jdtls.setup_dap({ hotcodereplace = "auto" })
-    jdtls.setup.add_commands()
-    local opts = { silent = true, buffer = bufnr }
-    vim.keymap.set('n', "<A-o>", jdtls.organize_imports, opts)
-    vim.keymap.set('n', "<leader>df", jdtls.test_class, opts)
-    vim.keymap.set('n', "<leader>dn", jdtls.test_nearest_method, opts)
-    vim.keymap.set('n', "crv", jdtls.extract_variable, opts)
-    vim.keymap.set('v', 'crm', [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], opts)
-    vim.keymap.set('n', "crc", jdtls.extract_constant, opts)
+config.on_init = function(client, _)
+    client.notify("workspace/didChangeConfiguration", { settings = config.settings })
+end
 
+-- LSP Settings for Java.
+config.on_attach = function(_, bufnr)
+    jdtls.setup_dap({ hotcodereplace = "auto" })
+    jdtls_dap.setup_dap_main_class_configs()
+    jdtls.setup.add_commands()
+
+    local opts = { silent = true, buffer = bufnr }
+    vim.keymap.set("n", "<A-o>", jdtls.organize_imports, opts)
+    vim.keymap.set("n", "<leader>df", jdtls.test_class, opts)
+    vim.keymap.set("n", "<leader>dn", jdtls.test_nearest_method, opts)
+    vim.keymap.set("n", "crv", jdtls.extract_variable, opts)
+    vim.keymap.set("v", "crm", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], opts)
+    vim.keymap.set("n", "crc", jdtls.extract_constant, opts)
+
+    -- Create a command `:Format` local to the LSP buffer.
     vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-      vim.lsp.buf.format()
+        vim.lsp.buf.format()
     end, { desc = "Format current buffer with LSP" })
 end
 
- config.on_init = function(client, _)
-    client.notify('workspace/didChangeConfiguration', { settings = config.settings })
-  end
+--[[
+
+
 local bundles = {
-	vim.fn.glob(
-		global.home_path .. "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.51.1.jar",
-		1
-	),
+    vim.fn.glob(
+        global.home_path
+        .. "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.51.1.jar",
+        1
+    ),
 }
 
 vim.list_extend(bundles, vim.split(vim.fn.glob(global.home_path .. "/vscode-java-test/server/*.jar", 1), "\n"))
-config["init_options"] = {
-	bundles = bundles,
+]]--
+
+print(vim.inspect(bundles))
+config.init_options = {
+    bundles = bundles,
+    extendedCapabilities = jdtls.extendedCapabilities,
 }
 
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
-jdtls.start_or_attach(config)
-
--- Commands
-vim.cmd([[ command! -buffer JdtUpdateDebugConfigs lua require('jdtls.dap').setup_dap_main_class_configs() ]])
+require('jdtls').start_or_attach(config)
