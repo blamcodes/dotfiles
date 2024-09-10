f6dcbfe1ddbba97283850ec22cb855631698c281
Bryan Lam <lamb6@carbon.macewan.ca>
2024-09-10 15:33:36 -0600

    Added hotkeys for gr and gi for lsp-config (go to reference and go to implementation)

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -35,6 +35,8 @@ return {
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
