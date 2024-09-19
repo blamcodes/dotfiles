0b223628c3caa8425bc6389cb7d306813b3d2df1
Bryan Lam <lamb6@carbon.macewan.ca>
2024-09-19 12:02:24 -0600

    Updates using lua patterns. Still not working...

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -6,18 +6,16 @@ return {
   },
   config = function()
     local lint = require("lint")
-    local vim = vim
-
 
     local function find_charts_dir(start_dir)
         local current_dir = start_dir or vim.fn.getcwd()
 
         while current_dir ~= '/' do
-            local charts_dir =  current_dir .. '/Charts'
+            local charts_dir =  current_dir .. '/Chart.yaml'
 
             if vim.loop.fs_stat(charts_dir) then
-                print(charts_dir)
-                return charts_dir
+                print(vim.fs.dirname(charts_dir))
+                return vim.fs.dirname(charts_dir)
             end
 
             current_dir = vim.fs.dirname(current_dir) -- Move up a directory
@@ -26,20 +24,34 @@ return {
         return nil
     end
 
-    -- Linter Custom
-    lint.linters.helmlint = {
-      cmd = 'helm lint',
+    local pattern = '%[(.+)%] (.+): (.+)'
+    local severity_map = {
+        ['ERROR'] = vim.diagnostic.severity.ERROR,
+        ['WARNING'] = vim.diagnostic.severity.WARN,
+        ['INFO'] = vim.diagnostic.severity.INFO,
+    }
+
+    local defaults = {
+        ["source"] = "helm"
+    }
+
+    local opts = {}
+
+    lint.linters.helm_lint = {
+      cmd = 'helm',
+      args = {'lint', function()
+          find_charts_dir(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+      end, '--quiet'},
       stdin = true,
-      stream = 'stdout', -- ('stdout' | 'stderr' | 'both') configure the stream to which the linter outputs the linting result.
-      ignore_exitcode = true, -- set this to true if the linter exits with a code != 0 and that's considered normal.
-      cwd = find_charts_dir(vim.fs.dirname(vim.api.nvim_buf_get_name(0))),
-      parser = function(output)
-        local diagnostics = {}
-        local ok, result = pcall(vim.json.decode, output)
-        if not ok then
-            return diagnostics
-        end
-      end
+      stream = 'stdout',
+      ignore_exitcode = true,
+      parser = require('lint.parser').from_pattern(
+        pattern,
+        { 'severity', 'file', 'message' },
+        severity_map,
+        defaults,
+        opts
+      )
     }
 
     lint.linters_by_ft = {
@@ -49,7 +61,7 @@ return {
       -- typescriptreact = { "eslint_d" },
       -- svelte = { "eslint_d" },
       -- python = { "pylint" },
-      helm = { "helmlint" }
+      helm = { "helm_lint" }
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
