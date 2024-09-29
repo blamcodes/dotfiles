35525acf8405c1d302455ec30bd1074fb7234528
Bryan Lam <lamb6@macewan.ca>
2024-09-29 09:25:39 -0600

    chore (nvim): update nvim-lsp plugin with additional config
    Config added for typescript and javascript

--- .config/nvim/lua/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -1,88 +1,102 @@
 return {
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
                     "jsonls",
                     "helm_ls",
                     "yamlls",
                     "phpactor",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+                    "eslint",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.tsserver.setup({
-				capabilties = capabilities,
-			})
+            lspconfig.tsserver.setup({
+                capabilties = capabilities,
+            })
 
-			-- lspconfig.jedi_language_server.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+            -- lspconfig.eslint.setup({
+            --     capabilties = capabilities,
+            --     on_attach = function(client, bufnr)
+            --         vim.api.nvim_create_autocmd("BufWritePre", {
+            --             buffer = bufnr,
+            --             command = "EslintFixAll",
+            --         })
+            --     end,
+            -- })
 
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
+            -- lspconfig.jedi_language_server.setup({
+            -- 	capabilities = capabilities,
+            -- })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.dockerls.setup({
+                capabilities = capabilities,
+            })
+
+            lspconfig.jsonls.setup({
+                capabilities = capabilities,
+            })
 
             lspconfig.phpactor.setup({
-				capabilities = capabilities,
+                capabilities = capabilities,
             })
-			--
-			--          lspconfig.jinja_lsp.setup({
-			-- 	capabilities = capabilities,
-			--          })
-			--
-			-- lspconfig.helm_ls.setup({
-			-- 	capabilities = capabilities,
-			--              -- settings = {
-			--              --     logLevel = "info",
-			--              --     valuesFile = {
-			--              --         mainValuesFile = "values.yaml",
-			--              --         lintOverlayValuesFile = "values.line.yaml",
-			--              --         additionalValuesFileGlobPattern = "values*.yaml"
-			--              --     },
-			--              --     yamlls = {
-			--              --         enabled = false,
-			--              --         enabledForFilesGlob = "*.{yaml,yml}",
-			--              --         diagnosticLimit = 50,
-			--              --         path = "yaml-language-server",
-			--              --         config = {
-			--              --             schemas = {
-			--              --                 kubernetes = "templates/**",
-			--              --             },
-			--              --             completion = true,
-			--              --             hover = true,
-			--              --         }
-			--              --     }
-			--              -- }
-			-- })
+            --
+            --          lspconfig.jinja_lsp.setup({
+            -- 	capabilities = capabilities,
+            --          })
+            --
+            -- lspconfig.helm_ls.setup({
+            -- 	capabilities = capabilities,
+            --              -- settings = {
+            --              --     logLevel = "info",
+            --              --     valuesFile = {
+            --              --         mainValuesFile = "values.yaml",
+            --              --         lintOverlayValuesFile = "values.line.yaml",
+            --              --         additionalValuesFileGlobPattern = "values*.yaml"
+            --              --     },
+            --              --     yamlls = {
+            --              --         enabled = false,
+            --              --         enabledForFilesGlob = "*.{yaml,yml}",
+            --              --         diagnosticLimit = 50,
+            --              --         path = "yaml-language-server",
+            --              --         config = {
+            --              --             schemas = {
+            --              --                 kubernetes = "templates/**",
+            --              --             },
+            --              --             completion = true,
+            --              --             hover = true,
+            --              --         }
+            --              --     }
+            --              -- }
+            -- })
 
-			-- lspconfig.yamlls.setup({
-			-- 	capabilities = capabilities,
-			-- })
+            -- lspconfig.yamlls.setup({
+            -- 	capabilities = capabilities,
+            -- })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
             vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
