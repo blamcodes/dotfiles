8dfba1c0afbd069bbbc3156a8a4eafb91c69289e
Bryan Lam <lamb6@macewan.ca>
2024-09-29 09:26:16 -0600

    fix (nvim): changed indent behaviour

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,19 @@
-return { 
+return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            highlight = { 
-                enable = true, 
+            highlight = {
+                enable = true,
                 --[[
                 disable = {
                     "markdown"
                 },
                 --]]
             },
-            indent = { enable = false },
+            indent = { enable = true },
             --ignore_install = { "markdown" }
         })
     end
