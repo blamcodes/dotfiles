931efc5e0e3f8a024e4ba4b309bf6c530f652847
Bryan Lam <lamb6@macewan.ca>
2024-09-29 09:21:49 -0600

    chore: formatting fix in none-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,28 +1,29 @@
 return {
-	"nvimtools/none-ls.nvim",
+    "nvimtools/none-ls.nvim",
     enabled = false,
-	config = function()
-		local null_ls = require("null-ls")
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
+        null_ls.setup({
+            sources = {
                 -- Lua
-				null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.stylua,
 
                 -- Javascript
-				-- @todo fix this: null_ls.builtins.diagnostics.eslint_d,
-				-- null_ls.builtins.formatting.prettier,
+                -- @todo fix this: null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.formatting.eslint_d,
+                -- null_ls.builtins.formatting.prettier,
 
                 -- Python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
 
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.yamllint,
 
-			},
-		})
+            },
+        })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
