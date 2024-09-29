0074a17a8531f1b765c7f88e79589db5c6b73a1e
Bryan Lam <lamb6@macewan.ca>
2024-09-29 09:27:24 -0600

    feat (nvim): added document_symbols search bound to <leader>fs

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,11 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>p', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fs', function()
+                builtin.lsp_document_symbols({
+                    ignore_symbols = { 'variables' }
+                })
+            end, {})
         end
     },
     {
