eba5b82de5ad9e0f40d6374d8dc771577d2e8a22
Bryan Lam <lamb6@macewan.ca>
2024-09-29 09:27:41 -0600

    chore (nvim): set telescope to use 'rg'

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim', 'dapc11/telescope-yaml.nvim' },
         config = function()
+            vim.g.telescope = 'rg'
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>p', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
