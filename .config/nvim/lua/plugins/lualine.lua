43a1fb4af7084d25e5751768990e9305cd9d238a
Bryan Lam <lamb6@macewan.ca>
2024-10-02 08:30:05 -0600

    chore (nvim): adjusted lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,15 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'dracula'
-            }
-        })
-    end
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'catppuccin'
+      },
+      sections = {
+        lualine_x = { 'encoding', 'fileformat', 'filetype', 'progress' },
+        lualine_y = { 'location' },
+        lualine_z = { 'datetime' }
+      }
+    })
+  end
 }
