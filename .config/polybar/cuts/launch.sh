f9d013a4716e17d86d0bbe7cf0364b3d6b1c29da
Bryan Lam <lamb6@macewan.ca>
2024-10-02 08:28:00 -0600

    chore (polybar): adjustment to polybar cuts theming

--- .config/polybar/cuts/launch.sh
+++ .config/polybar/cuts/launch.sh
@@ -10,6 +10,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q top -c "$DIR"/config.ini &
-polybar -q bottom -c "$DIR"/config.ini &
+if type "xrandr"; then
+  # for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  #   MONITOR=$m polybar -q top -c "$DIR"/config.ini &
+  # done
+  MONITOR="DP-3-8" polybar -q top -c "$DIR"/config.ini &
+  MONITOR="eDP-1" polybar -q alt -c "$DIR"/config.ini &
+  MONITOR="DP-3-1" polybar -q alt -c "$DIR"/config.ini &
+else
+  polybar -q top -c "$DIR"/config.ini &
+fi
+
+# # Launch the bar
+# polybar -q top -c "$DIR"/config.ini &
