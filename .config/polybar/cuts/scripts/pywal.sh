66093522197cb99f97a5b996e93ce18113675e16
Bryan Lam <lamb6@macewan.ca>
2024-10-06 10:43:15 -0600

    chore (polybar & pywal): update colors to be more aesthetic

--- .config/polybar/cuts/scripts/pywal.sh
+++ .config/polybar/cuts/scripts/pywal.sh
@@ -14,10 +14,28 @@ pywal_get() {
 change_color() {
 	# polybar
 	sed -i -e "s/background = #.*/background = #${BG}/g" $PFILE
-	sed -i -e "s/background-alt = #.*/background-alt = #8C${BG}/g" $PFILE
+	sed -i -e "s/background-alt = #.*/background-alt = #${BGA}/g" $PFILE
 	sed -i -e "s/foreground = #.*/foreground = #${FG}/g" $PFILE
-	sed -i -e "s/foreground-alt = #.*/foreground-alt = #33${FG}/g" $PFILE
-	sed -i -e "s/primary = #.*/primary = $AC/g" $PFILE
+	sed -i -e "s/foreground-alt = #.*/foreground-alt = #${FGA}/g" $PFILE
+	sed -i -e "s/primary = #.*/primary = ${BG}/g" $PFILE
+  sed -i -e "s/red = #.*/red = ${RED}/g" $PFILE
+  sed -i -e "s/green = #.*/green = ${GREEN}/g" $PFILE
+  sed -i -e "s/yellow = #.*/yellow = ${YELLOW}/g" $PFILE
+	sed -i -e "s/color1 = .*/color1 = ${COLOR1}/g" $PFILE
+	sed -i -e "s/color2 = .*/color2 = ${COLOR2}/g" $PFILE
+	sed -i -e "s/color3 = .*/color3 = ${COLOR3}/g" $PFILE
+	sed -i -e "s/color4 = .*/color4 = ${COLOR4}/g" $PFILE
+	sed -i -e "s/color5 = .*/color5 = ${COLOR5}/g" $PFILE
+	sed -i -e "s/color6 = .*/color6 = ${COLOR6}/g" $PFILE
+	sed -i -e "s/color7 = .*/color7 = ${COLOR7}/g" $PFILE
+	sed -i -e "s/color8 = .*/color8 = ${COLOR8}/g" $PFILE
+	sed -i -e "s/color9 = .*/color9 = ${COLOR9}/g" $PFILE
+	sed -i -e "s/color10 = .*/color10 = ${COLOR10}/g" $PFILE
+	sed -i -e "s/color11 = .*/color11 = ${COLOR11}/g" $PFILE
+	sed -i -e "s/color12 = .*/color12 = ${COLOR12}/g" $PFILE
+	sed -i -e "s/color13 = .*/color13 = ${COLOR13}/g" $PFILE
+	sed -i -e "s/color14 = .*/color14 = ${COLOR14}/g" $PFILE
+	sed -i -e "s/color15 = .*/color15 = ${COLOR15}/g" $PFILE
 	
 	# rofi
 	cat > $RFILE <<- EOF
@@ -26,10 +44,10 @@ change_color() {
 	* {
 	  al:   #00000000;
 	  bg:   #${BG}BF;
-	  bga:  #${BG}FF;
+	  bga:  #${BGA}FF;
 	  fg:   #${FG}FF;
-	  ac:   ${AC}FF;
-	  se:   ${AC}1A;
+	  ac:   ${RED}FF;
+	  se:   ${COLOR12}1A;
 	}
 	EOF
 	
@@ -48,16 +66,41 @@ if [[ -x "`which wal`" ]]; then
 			echo 'Color file does not exist, exiting...'
 			exit 1
 		fi
+    echo "hello"
+    echo $background
+    echo $background
+    echo $color1
 
 		BGC=`printf "%s\n" "$background"`
 		BG=${BGC:1}
-		FGC=`printf "%s\n" "$foreground"`
+		BGAC=`printf "%s\n" "$foreground"`
+		BGA=${BGAC:1}
+		FGC=`printf "%s\n" "$background"`
 		FG=${FGC:1}
-		AC=`printf "%s\n" "$color1"`
+		FGAC=`printf "%s\n" "$color7"`
+		FGA=${FGAC:1}
+		COLOR1=`printf "%s\n" "$color1"`
+		COLOR2=`printf "%s\n" "$color2"`
+		COLOR3=`printf "%s\n" "$color3"`
+		COLOR4=`printf "%s\n" "$color4"`
+		COLOR5=`printf "%s\n" "$color5"`
+		COLOR5=`printf "%s\n" "$color5"`
+		COLOR6=`printf "%s\n" "$color6"`
+		YELLOW=`printf "%s\n" "$color6"`
+		COLOR7=`printf "%s\n" "$color7"`
+		COLOR8=`printf "%s\n" "$color8"`
+		RED=`printf "%s\n" "$color8"`
+		COLOR9=`printf "%s\n" "$color9"`
+		COLOR10=`printf "%s\n" "$color10"`
+		COLOR11=`printf "%s\n" "$color11"`
+		GREEN=`printf "%s\n" "$color5"`
+		COLOR12=`printf "%s\n" "$color12"`
+		COLOR13=`printf "%s\n" "$color13"`
+		COLOR14=`printf "%s\n" "$color14"`
+		COLOR15=`printf "%s\n" "$color15"`
 
 		change_color
-	else
-		echo -e "[!] Please enter the path to wallpaper. \n"
+	else echo -e "[!] Please enter the path to wallpaper. \n"
 		echo "Usage : ./pywal.sh path/to/image"
 	fi
 else
