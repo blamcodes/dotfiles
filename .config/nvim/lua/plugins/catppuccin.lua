122225070336153d5d9d75245088e7f3f1d30c48
Bryan Lam <bryan.lam@ironsight.app>
2025-01-23 13:54:19 -0700

    chore(nvim): formatting in catppuccin.lua

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,49 +1,50 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    local catppuccin = require("catppuccin")
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        local catppuccin = require("catppuccin")
 
-    catppuccin.setup({
-      flavour = "macchiato",
-      transparent_background = true,
-      -- integrations = {
-      --     telescope = {
-      --         enabled = true,
-      --         style = "nvchad"
-      --     }
-      -- }
-    })
+        catppuccin.setup({
+            flavour = "macchiato",
+            transparent_background = true,
+            styles = {
+                comments = { "italic" },
+                functions = { "italic" },
+            },
+            integrations = {
+                treesitter = true,
+            }
+        })
 
-    local colors = require("catppuccin.palettes").get_palette()
-    local TelescopeColor = {
-      TelescopeMatching = { fg = colors.rosewater },
-      TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+        local colors = require("catppuccin.palettes").get_palette()
+        local TelescopeColor = {
+            TelescopeMatching = { fg = colors.rosewater },
+            TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
 
-      TelescopePromptPrefix = { bg = "NONE" },
-      TelescopePromptNormal = { bg = "NONE" },
-      TelescopeResultsNormal = { bg = "NONE" },
-      TelescopePreviewNormal = { bg = "NONE" },
+            TelescopePromptPrefix = { bg = "NONE" },
+            TelescopePromptNormal = { bg = "NONE" },
+            TelescopeResultsNormal = { bg = "NONE" },
+            TelescopePreviewNormal = { bg = "NONE" },
 
-      TelescopePromptBorder = { bg = "NONE", fg = colors.mantle },
-      TelescopeResultsBorder = { bg = "NONE", fg = colors.mantle },
-      TelescopePreviewBorder = { bg = "NONE", fg = colors.mantle },
+            TelescopePromptBorder = { bg = "NONE", fg = colors.mantle },
+            TelescopeResultsBorder = { bg = "NONE", fg = colors.mantle },
+            TelescopePreviewBorder = { bg = "NONE", fg = colors.mantle },
 
-      TelescopePromptTitle = { bg = colors.mauve, fg = colors.mantle },
-      TelescopeResultsTitle = { bg = colors.green, fg = colors.mantle },
-      TelescopePreviewTitle = { bg = colors.pink, fg = colors.mantle },
-    }
+            TelescopePromptTitle = { bg = colors.mauve, fg = colors.mantle },
+            TelescopeResultsTitle = { bg = colors.green, fg = colors.mantle },
+            TelescopePreviewTitle = { bg = colors.pink, fg = colors.mantle },
+        }
 
-    for hl, col in pairs(TelescopeColor) do
-      vim.api.nvim_set_hl(0, hl, col)
-    end
+        for hl, col in pairs(TelescopeColor) do
+            vim.api.nvim_set_hl(0, hl, col)
+        end
 
-    vim.cmd.colorscheme("catppuccin")
-    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = "#939ab7", bold = false })
-    vim.api.nvim_set_hl(0, 'CursorLine', { bg = colors.mantle, fg = "NONE", bold = false })
-    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "#939ab7", bold = true })
-    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = "#939ab7", bold = false })
-    vim.api.nvim_set_hl(0, 'Folded', { fg = "#5b6078", bold = false })
-  end,
+        vim.cmd.colorscheme("catppuccin")
+        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = "#939ab7", bold = false })
+        vim.api.nvim_set_hl(0, 'CursorLine', { bg = colors.mantle, fg = "NONE", bold = false })
+        vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "#939ab7", bold = true })
+        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = "#939ab7", bold = false })
+        vim.api.nvim_set_hl(0, 'Folded', { fg = "#5b6078", bold = false })
+    end,
 }
