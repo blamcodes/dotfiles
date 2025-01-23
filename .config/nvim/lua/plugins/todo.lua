d536847c8abd19641f0ca7c2021cca4d6d9c79ba
Bryan Lam <bryan.lam@ironsight.app>
2025-01-23 13:56:29 -0700

    feat(nvim): add todo-comment

---
+++ .config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            keywords = {
+                QUESTION = { icon = " ", color = "#00FF00" }
+            }
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    }
+}
