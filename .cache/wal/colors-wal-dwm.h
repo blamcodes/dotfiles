5b91007c533adaa9eea17232278124034e5d4302
Bryan Lam <bryan.lam@ironsight.app>
2025-01-23 13:46:59 -0700

    chore: uncomment cs.lua

--- .cache/wal/colors-wal-dwm.h
+++ .cache/wal/colors-wal-dwm.h
@@ -1,14 +1,14 @@
-static const char norm_fg[] = "#dfc7d9";
-static const char norm_bg[] = "#0B0A13";
-static const char norm_border[] = "#9c8b97";
+static const char norm_fg[] = "#acb5ba";
+static const char norm_bg[] = "#070710";
+static const char norm_border[] = "#787e82";
 
-static const char sel_fg[] = "#dfc7d9";
-static const char sel_bg[] = "#60388A";
-static const char sel_border[] = "#dfc7d9";
+static const char sel_fg[] = "#acb5ba";
+static const char sel_bg[] = "#9C3F4E";
+static const char sel_border[] = "#acb5ba";
 
-static const char urg_fg[] = "#dfc7d9";
-static const char urg_bg[] = "#87607A";
-static const char urg_border[] = "#87607A";
+static const char urg_fg[] = "#acb5ba";
+static const char urg_bg[] = "#5D5C6B";
+static const char urg_border[] = "#5D5C6B";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
