44a9ba4933ff0d08d38b9c2d4a09e9c615b33f08
Bryan Lam <bryan.lam@ironsight.app>
2025-01-23 13:49:24 -0700

    feat(nvim): added fzf-lua

---
+++ .config/nvim/lua/plugins/fzflua.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "ibhagwan/fzf-lua",
+        -- optional for icon support
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- or if using mini.icons/mini.nvim
+        -- dependencies = { "echasnovski/mini.icons" },
+        opts = {},
+        config = function()
+            local fzf = require("fzf-lua")
+
+            fzf.setup({ "fzf-native" })
+
+            vim.keymap.set('n', '<leader>p', fzf.files, {})
+            -- vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>fg', fzf.live_grep, {})
+            vim.keymap.set('n', '<leader>fc', fzf.lgrep_curbuf, {})
+            -- vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+            vim.keymap.set('n', '<leader>fb', fzf.buffers, {})
+            vim.keymap.set('n', '<leader>fq', ":TodoFzfLua<CR>", {})
+            -- vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+            -- vim.keymap.set('n', '<leader>fd', function()
+            --     builtin.lsp_document_symbols({
+            --         symbols = { 'function', 'method', 'variable' }
+            --     })
+            -- end, {})
+            -- vim.keymap.set('n', '<leader>fw', function()
+            --     builtin.lsp_workspace_symbols({
+            --         symbols = { 'function', 'class' }
+            --     })
+            -- end, {})
+        end
+    }
+}
