d87962aeb287b9e2e9308a67a778f1ca3faa0017
Bryan Lam <bryan.lam@ironsight.app>
2025-01-23 13:59:50 -0700

    feat(nvim): add other.lua
    Supposedly able to set a "other" file for test file for quick switching.

---
+++ .config/nvim/lua/plugins/other.lua
@@ -0,0 +1,19 @@
+return {
+    'rgroli/other.nvim',
+    config = function()
+        -- Check https://github.com/rgroli/other.nvim
+
+        -- mappings = {
+        --     {
+        --         pattern = "/src/app/(.*)/.*.ts$",
+        --         target = "/src/app/%1/%1.component.html",
+        --         context = "component" -- optional
+        --     },
+        --     {
+        --         pattern = "/src/app/(.*)/.*.html$",
+        --         target = "/src/app/%1/%1.component.ts",
+        --         context = "view"
+        --     }
+        -- }
+    end
+}
