6a2a7c498d8b9f4787b9b6a718551c8c25d4dd8a
Bryan Lam <bryan.lam@ironsight.app>
2025-02-01 11:38:52 -0700

    feat(nvim): add autocommand to go to last location when opening a buffer

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,3 +1,7 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     if require("nvim-treesitter.parsers").has_parser() then
@@ -5,6 +9,24 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
     else
       vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
   end,
 })
